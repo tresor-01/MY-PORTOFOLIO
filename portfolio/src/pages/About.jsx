91684d96640dfcc6fe71eaec18152aa3fb8bc313
tresor-01 <tresoralain3@gmail.com>
2025-10-19 11:01:30 +0400

    feat: import header & footer

--- portfolio/src/pages/About.jsx
+++ portfolio/src/pages/About.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Sidebar from '../components/Sidebar'
-
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import Sidebar from '../components/Sidebar';
 const About = () => {
   return (
     <>
+    <Header/>
     <div className =''>About</div>
     <Sidebar/>
+
+    <Footer />
     </>
   )
 }
